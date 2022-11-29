3995518189a49fc56b7daf7879f720bf2db26b00
Preet <preetbhatia088@gmail.com>
2022-11-29 23:08:11 +0530

    website code for minor

---
+++ doctors website/script1.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".mul-select").select2({
+        placeholder: "Select all the relevant symtoms",
+        tags: true,
+    });
+})
+
+var Headche   = document.getElementById("title").value;
+var myArray = [ Headche];
+print(myArray)
